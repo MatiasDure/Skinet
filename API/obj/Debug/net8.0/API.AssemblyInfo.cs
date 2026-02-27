22d9ea1320dca05dbc8948f496357a2555ba405d
Matias Dure <matidure1997@gmail.com>
2026-02-27 14:48:04 +0100

    Create initial db migration

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f833cd621e6ea4e92439cb7f20f93901227c00b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc1d9a65e76d82bfd495f9913a900d913146d0ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -8,4 +8,10 @@ public class StoreContext : DbContext
 {
     public DbSet<Product> Products { get; set; }
     public StoreContext(DbContextOptions options) : base(options) {}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(StoreContext).Assembly);
+    }
 }
