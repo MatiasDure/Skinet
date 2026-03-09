59d739579ad96316e01de884ae5697a7aee61ba4
Matias Dure <matidure1997@gmail.com>
2026-03-09 08:44:24 +0100

    updated docker compose to using old volume after path update

--- Backend/Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Backend/Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e28a520c42e95e89e08434de0d1c2bee7dd0d94a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7f22e72e02e90c6164fd0b5cccdd046076af20c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
